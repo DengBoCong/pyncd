ae38e879d40291dbd86edabe6480d20b25ab3a2a
DengBoCong <1210212670@qq.com>
2023-01-17 12:49:38 +0800

    fix example

--- pyncd/models/louvain.py
+++ pyncd/models/louvain.py
@@ -112,7 +112,7 @@ class LouvainDetector(BaseDetector):
         >>> import networkx as nx
         >>> from pyncd.models.louvain import LouvainDetector
         >>> G = nx.petersen_graph()
-        >>> detector = LouvainDetector
+        >>> detector = LouvainDetector()
         >>> detector.fit(G)
         >>> detector.decision_function([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
         [0, 1, 1, 1, 0, 0, 1, 0, 1, 0]
